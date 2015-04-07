f25f0d31d1fe16f28ab88167b7fc52bc1118f3a2
nachoJcastro <nachoj.castro@gmail.com>
2015-04-06 22:32:28 -0300

    dal employee

--- DataAccessLayer/DALEmployeesEF.cs
+++ DataAccessLayer/DALEmployeesEF.cs
@@ -7,30 +7,17 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    public class DALEmployeesEF : IDALEmployees
+     public class DALEmployeesEF : IDALEmployees
     {
+        static EmployeeDBContext db = new EmployeeDBContext();
+        
         // agrego empleado
         public void AddEmployee(Employee emp)
         {
-
-            throw new NotImplementedException();
-        }
-
-
-        // borro empleado
-        public static void DeleteEmployee(int id)
-        {
-            EmployeeDBContext db = new
-            EmployeeDBContext();
-
-            var matched_Employee = (from c in db.GetTable<Employee>()
-                                    where c.id == id
-                                    select c).SingleOrDefault();
-
             try
             {
-                db.Employee.DeleteOnSubmit(matched_Employee);
-                db.SubmitChanges();
+                db.Employee.Add(emp);
+                db.SaveChangesAsync();
             }
             catch (Exception ex)
             {
@@ -38,28 +25,94 @@ namespace DataAccessLayer
             }
         }
 
+        // borro empleado
+        public static void DeleteEmployee(int id)
+        {
+             var matched_Employee = (from c in db.Employee
+                                    where c.Id == id
+                                    select c).SingleOrDefault();
+             try
+            {
+                db.Employee.Remove(matched_Employee);
+                db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
 
-
-
-
+        // actualizo empleado
         public void UpdateEmployee(Employee emp)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var empleado = db.Employee
+                    .Where(w => w.Id == emp.Id)
+                    .SingleOrDefault();
+
+                if (emp != null)
+                {
+                    empleado.Name = emp.Name;
+                    empleado.StartDate = emp.StartDate;
+                    db.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
         }
 
+        // listo empleado
         public List<Employee> GetAllEmployees()
         {
-            throw new NotImplementedException();
+            List<Employee> empleados = new List<Employee> ;   
+            try
+            {
+                empleados = db.Employee.ToList();
+                return empleados;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
+        // obtengo empleado
         public Employee GetEmployee(int id)
         {
-            throw new NotImplementedException();
+            Employee emp = null;
+            
+            try
+            {
+                 emp = db.Employee
+                    .Where(w => w.Id == id)
+                    .SingleOrDefault();
+
+                 return emp;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
+        // busco empleados    
         public List<Employee> SearchEmployees(string searchTerm)
         {
-            throw new NotImplementedException();
+            List<Employee> empleados = new List<Employee> ;   
+            try
+            {
+                empleados =  db.Employee
+                    .Where(w => w.Name == searchTerm).ToList();
+                return empleados;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
