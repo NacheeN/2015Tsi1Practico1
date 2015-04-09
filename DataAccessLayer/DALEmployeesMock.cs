a2a93b88eb05fe2e75de63cebd13c9a45d498cb1
ialamon@gilums.com <ialamon@gilums.com>
2015-04-08 21:13:34 -0300

    Cambios en DALEMployeeMock

--- DataAccessLayer/DALEmployeesMock.cs
+++ DataAccessLayer/DALEmployeesMock.cs
@@ -167,6 +167,45 @@ namespace DataAccessLayer
             }
         }
 
+
+        public void DeleteListEmployee(List<Employee> emps)
+        {
+            var list = from e in db.Employee
+                       orderby e.Id ascending
+                       select e;
+
+            try
+            {
+                foreach (Employee e in employeesRepository)
+                {
+                    db.Employee.Remove(e);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+
+        public List<FullTimeEmployee> GetAllEmployeeSalaryAbove(int minsalary)
+        {
+            List<FullTimeEmployee> listfull = new List<FullTimeEmployee>();
+
+            try
+            {
+                var list = listfull.Where<FullTimeEmployee>(e => e.Salary >= minsalary).ToList();
+                   
+                return listfull;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
     }
 
        
